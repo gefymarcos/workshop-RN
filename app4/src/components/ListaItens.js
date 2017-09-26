efa9c69de2be7fd863b839152319aa6b92c73f79
Gefy Marcos <gefymarcos@gmail.com>
2017-09-26 18:50:35 -0300

    lista de itens

--- app4/src/components/ListaItens.js
+++ app4/src/components/ListaItens.js
@@ -2,24 +2,35 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
+  Text
 } from 'react-native';
 import axios from 'axios';
 import Itens from './Itens';
 
 export default class ListaItens extends Component {
   
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      listaItens: []
+    };
+  }
+  
   componentWillMount() {
     axios.get('https://gefymarcos.github.io/itens.html')
-      .then(res => console.log(res))
+      .then(res => this.setState({
+        listaItens: res.data
+      }))
       .catch(e => console.log(e));
   }
 
   render() {
     return (
       <View>
-        <Itens />
-        <Itens />
-        <Itens />
+        { 
+          this.state.listaItens.map(item => { return (<Text>{item.titulo}</Text>)})
+        }
       </View>
     );
   }
